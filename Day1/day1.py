dd94279ed0e23ec59c39380a2c70cc1f3e4534ff
Louise <louiseli@deloitte.co.uk>
2022-12-02 08:49:39 +0000

    Fix print

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,6 +1,6 @@
-last_elf_calories = current_calories = max_calories = elf = 0
+last_elf_calories = current_calories = max_calories = elf = max_elf = 0
 list = []
-with open ('./day1input.txt', 'r') as file:
+with open ('/Users/louiseli/AdventOfCode2022/Day1/day1input.txt', 'r') as file:
     lines = file.readlines()
     for line in lines:
         if line != '\n':
@@ -10,7 +10,7 @@ with open ('./day1input.txt', 'r') as file:
             list.append(current_calories)
             if current_calories > max_calories:
                 max_calories = current_calories
+                max_elf = elf
             current_calories = 0
-    print ("Top elf number", elf, " with ", max_calories, " calories!")
+    print ("Top elf number", max_elf, " with ", max_calories, " calories!")
     print("Top 3 elves total calories: ", sum((sorted(list))[-3:]))
-
